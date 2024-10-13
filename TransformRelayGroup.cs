a987147d6a3f40d2d375538404e51058ed9a64ce
exerussus <solovyov.production@gmail.com>
2024-10-13 04:55:53 +0300

    Auto updated to version 1.0.6

--- TransformRelayGroup.cs
+++ TransformRelayGroup.cs
@@ -1,5 +1,6 @@
 ﻿
 using ECS.Modules.Exerussus.TransformRelay.Systems;
+using Exerussus._1EasyEcs.Scripts.Core;
 using Exerussus._1EasyEcs.Scripts.Custom;
 using Leopotam.EcsLite;
 
@@ -12,6 +13,18 @@ namespace ECS.Modules.Exerussus.TransformRelay
         protected override void SetFixedUpdateSystems(IEcsSystems fixedUpdateSystems)
         {
             fixedUpdateSystems.Add(new TransformRelaySystem(Settings));
+        }
+        
+        public TransformRelayGroup SetUpdateType(UpdateType updateType)
+        {
+            Settings.Update = updateType;
+            return this;
+        }
+        
+        public TransformRelayGroup SetPositionOriginMode(bool isEnabled)
+        {
+            Settings.IsPositionOrigin = isEnabled;
+            return this;
         }
     }
 }
