6c0c812cad64212e181fe74aa58c44b0d2667df4
exerussus <solovyov.production@gmail.com>
2024-10-28 00:35:42 +0300

    Auto updated to version 1.0.8

--- TransformRelayPooler.cs
+++ TransformRelayPooler.cs
@@ -1,7 +1,9 @@
 ﻿
+using ECS.Modules.Exerussus.Movement;
 using Exerussus._1EasyEcs.Scripts.Core;
 using Exerussus._1EasyEcs.Scripts.Custom;
 using Leopotam.EcsLite;
+using UnityEngine;
 
 namespace ECS.Modules.Exerussus.TransformRelay
 {
@@ -12,6 +14,15 @@ namespace ECS.Modules.Exerussus.TransformRelay
             Transform = new PoolerModule<TransformRelayData.Transform>(world);
         }
 
+        [InjectSharedObject] private MovementPooler _movementPooler;
         public PoolerModule<TransformRelayData.Transform> Transform { get; private set; }
+
+        public void CreateBaseData(int entity, Transform transform)
+        {
+            ref var positionData = ref _movementPooler.Position.Add(entity);
+            positionData.Value = transform.position;
+            ref var transformRelayData = ref Transform.Add(entity);
+            transformRelayData.Value = transform;
+        }
     }
 }
